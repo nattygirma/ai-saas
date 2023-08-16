749a91e97e3d099335aef8498c522519ebd52779
nattygirma <nattygirma28@gmail.com>
2023-08-16 18:03:49 +0530

    first commit

--- app/(dashboard)/(routes)/music/page.tsx
+++ app/(dashboard)/(routes)/music/page.tsx
@@ -46,7 +46,7 @@ const MusicPage = () => {
       if (error?.response?.status === 403) {
         proModal.onOpen();
       } else {
-        toast.error("Something went wrong.");
+        toast.error("Something went wrong!");
       }
     } finally {
       router.refresh();
