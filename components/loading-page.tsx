553ec498b97c448fd550870a646f2cb4df832220
nattygirma <nattygirma28@gmail.com>
2023-08-16 17:08:10 +0530

    first commit

--- components/loading-page.tsx
+++ components/loading-page.tsx
@@ -1,29 +1,29 @@
-"use client";
+// "use client";
 
-import { useEffect, useState } from "react";
-// import { useRouter } from 'next/router'
-import { useRouter } from 'next/navigation'
+// import { useEffect, useState } from "react";
+// // import { useRouter } from 'next/router'
+// import { useRouter } from 'next/navigation'
 
-export const Loading = () => {
+// export const Loading = () => {
 
-    const router = useRouter();
+//     const router = useRouter();
 
-    const [loading, setLoading] = useState(false);
+//     const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        const handleStart = (url) => (url !== router?.asPath) && setLoading(true);
-        const handleComplete = (url) => (url === router?.asPath) && setLoading(false);
+//     useEffect(() => {
+//         const handleStart = (url) => (url !== router?.asPath) && setLoading(true);
+//         const handleComplete = (url) => (url === router?.asPath) && setLoading(false);
 
-        router?.events?.on('routeChangeStart', handleStart)
-        router?.events?.on('routeChangeComplete', handleComplete)
-        router?.events?.on('routeChangeError', handleComplete)
+//         router?.events?.on('routeChangeStart', handleStart)
+//         router?.events?.on('routeChangeComplete', handleComplete)
+//         router?.events?.on('routeChangeError', handleComplete)
 
-        return () => {
-            router?.events?.off('routeChangeStart', handleStart)
-            router?.events?.off('routeChangeComplete', handleComplete)
-            router?.events?.off('routeChangeError', handleComplete)
-        }
-    })
+//         return () => {
+//             router?.events?.off('routeChangeStart', handleStart)
+//             router?.events?.off('routeChangeComplete', handleComplete)
+//             router?.events?.off('routeChangeError', handleComplete)
+//         }
+//     })
     
-    return loading && (<div><h1>Loading....</h1></div>);
-}
+//     return loading && (<div><h1>Loading....</h1></div>);
+// }
